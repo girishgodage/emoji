5794e9ba92a307162ba034c8b4587e2c6e613b9e
girish <girishgodage@gmail.com>
2021-01-16 20:10:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { PureComponent } from "react";
+import Header from './components/Header/Header';
+import SearchInput from "./components/SearchInput/SearchInput";
+import EmojiResults from "./components/EmojiResults/EmojiResults";
+import filterEmoji from "./components/filterEmoji/filterEmoji";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredEmoji: filterEmoji("", 20)
+    };
+  }
+
+  handleSearchChange = event => {
+    this.setState({
+      filteredEmoji: filterEmoji(event.target.value, 20)
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <SearchInput textChange={this.handleSearchChange} />
+        <EmojiResults emojiData={this.state.filteredEmoji} />
+      </div>
+    );
+  }
 }
 
-export default App;
